0faf95b100037b7607c18ff7952c19856bdc397e
carlos-705 <munozcarlos705@gmail.com>
2023-07-15 13:27:52 -0500

    ES12, ejemplos de la mejores en esta actualizacion

---
+++ src/es12/01-replaceall.js
@@ -0,0 +1,4 @@
+const string = "Hello world! I'm going to learn JScript";
+
+const replace = string.replace("JScript","TypeScript");
+console.log(replace);
---
+++ src/es12/03-private.js
@@ -0,0 +1,68 @@
+
+class User{
+    //metodos
+    greeting(){
+        return 'Hello';
+    }
+};
+
+const gndx = new User();
+console.log(gndx.greeting);
+const bebeloper = new User();
+console.log(bebeloper.greeting);
+
+//constructor   
+
+class user{
+    //contructor 
+    constructor(){
+        console.log('Nuevo Usuario');
+    };
+    greeting(){
+        return 'Hello';
+    };
+}
+const david = new user();
+
+//this 
+class user {
+    constructor(name){
+        this.name = name;
+    };
+    speak(){
+        return 'Hello';
+    };
+    greeting(){
+        return `${this.speak()} ${this.name}`;
+    };
+}
+
+const ana = new user('Ana');
+console.log(ana.greeting);
+
+//setters and getters
+
+class user {
+    //metodo constructor
+    constructor(name,age){
+        this.name = name;
+        this.age = age;
+    };
+    //metodos 
+    #speak(){ // # se vuelve privada para trabajar solo en funciones locales.
+        return 'Hello';
+    };
+    greeting(){
+        return `${this.speak()} ${this.name}`;
+    };
+    get #uAge(){
+        return this.age;
+    };
+    set #uAge(n){
+        this.age = n;
+    };
+}
+
+const bebeloper1 = new user('Carlos',15);
+console.log(bebeloper1.uAge);
+console.log(bebeloper1.uAge = 20);
